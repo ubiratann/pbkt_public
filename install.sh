6202278f0b189605d0ad32703e098fc6dc2a7165
Ubiratan Junior <45007635+ubiratann@users.noreply.github.com>
2023-11-11 21:17:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+echo ${PYTHON_PATH} -m ensurepip --upgrade
 
-$PYTHON_PATH -m ensurepip --upgrade
+mkdir -p /tmp/pbkt-installation
 
-mkdir -p tmp/pbkt-installation
-
-cd tmp/pbkt-installation
+cd /tmp/pbkt-installation
 
 curl -L -O https://githubraw.com/ubiratann/PBKT/main/main.py  
 sed -i 's|#!/bin/python3.10|#!'${PYTHON_PATH}'|' main.py
 
-curl -L -O https://githubraw.com/ubiratann/PBKT/main/requirements.txt  
-$PYTHON_PATH -m pip install -r requirements.txt
+curl -L -O /tmp https://githubraw.com/ubiratann/PBKT/main/requirements.txt  
+echo ${PYTHON_PATH} -m pip install -r requirements.txt
 
-mv main.py tmp/usr/bin/pbkt
-chmod a=rx tmp/usr/bin/pbkt
+echo mv main.py tmp/usr/bin/pbkt
+echo chmod a=rx tmp/usr/bin/pbkt
